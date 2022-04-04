7c640e102b56107915aba0b450c82a32dd1fa8eb
TonerikoF <Apife24@gmail.com>
2022-04-04 20:00:21 +0300

    HW_3 login

---
+++ Java-example/src/test/java/HW_3.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class HW_3 {
+    public WebDriver driver;
+    @Test
+    public void login () {
+        driver = new ChromeDriver();
+
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.quit();
+    }
+}
